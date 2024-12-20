11029d85a57f5d65a513f87967d1d3f1d011ae73
aishwinj04 <aishwin@gmail.com>
2024-12-19 23:45:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ import os
 my_email = os.getenv('email')
 my_password = os.getenv('password')
 
-subject = "Stock Price Notification!"
 yag = yagmail.SMTP(user=my_email, password=my_password)
+
+subject = "Stock Price Notification for Aishwin"
+
+content = " "
+
+yag.send(to=my_email, subject=subject, contents=content)
+
+def getContents():
+    
+
+
+
+
 
 
 
