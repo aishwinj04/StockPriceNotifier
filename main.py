3fee19a8cce5eebbf6aa1afeb9ea479cdbc23763
aishwinj04 <aishwin@gmail.com>
2024-12-19 23:42:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import yagmail 
 import os
 
-def sendEmail():
-    my_email = os.getenv('email')
-    my_password = os.getenv('password')
+my_email = os.getenv('email')
+my_password = os.getenv('password')
+
+subject = "Stock Price Notification!"
+yag = yagmail.SMTP(user=my_email, password=my_password)
+
+
+
