0f78aa07b29408f2faf672d5cfdd6c0e2e0aea3f
aishwinj04 <aishwin@gmail.com>
2024-12-20 01:14:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_driver():
     options.add_argument("disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome(options=options)
 
-    driver.get("https://www.marketwatch.com/investing/index/spx?mod=home-page")
+    driver.get("https://www.marketwatch.com/investing/stock/tsla")
 
     return driver
 
@@ -28,7 +28,7 @@ def clean_text(text):
 
 
 
-def send_email():
+def send_email(content):
     my_email = os.getenv('email')
     my_password = os.getenv('password')
 
@@ -43,11 +43,27 @@ def send_email():
 
 def main():
     driver = get_driver()
-    element = driver.find_element(
+    percent = driver.find_element(
         by="xpath", value= '//*[@id = "maincontent"]/div[2]/div[3]/div/div[2]/bg-quote/span[2]')
+    price = driver.find_element(by="xpath", value='//*[@id="maincontent"]/div[2]/div[3]/div/div[2]/h2/bg-quote')
+    print(price)
+    value = clean_text(percent.text)
+
     
-    cleaned = clean_text(element.text)
-    print(cleaned)
+    # if value < -10: 
+    #     content = f"""
+    #     We would like to inform you that Tesla's stock has experienced a significant decrease of 10% or more in value as of today. This represents a notable shift in the market, and we wanted to keep you updated on this development.
+    #     As always, we recommend reviewing your portfolio and considering any necessary adjustments in light of this change. If you have any questions or would like further details, feel free to reach out to us.
+    #     Thank you for your attention to this important update. We will continue to monitor the stock and provide you with timely notifications of any further changes.
+
+    #     The current price of the stock is: {}
+    #     Best regards,
+    #     StockNotifier Team
+    #     """
+    #     send_email(content)
+
+    
+    
 
 
 if __name__ == "__main__":
