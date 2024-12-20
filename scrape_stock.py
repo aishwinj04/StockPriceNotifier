056df0443bc1da64dd67abe7dd3d2c5cbf326c00
aishwinj04 <168397917+aishwinj04@users.noreply.github.com>
2024-12-20 02:38:17 -0500

    Update scrape_stock.py

--- scrape_stock.py
+++ scrape_stock.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-from datetime import datetime as dt
+
 
 # initialize webdriver to access site
 def get_driver():
